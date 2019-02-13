49f06600ce859c3bf3fade094530db057ac20cd5
arbhassan <hassanjawad1321@gmail.com>
2019-02-13 10:11:58 +0500

    Completed Setup

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { hot } from 'react-hot-loader'
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 
 class HelloWorld extends Component {
   render() {
     return (
-        <div>
-          <h1>Hello World!</h1>
-        </div>
-      )
+      <div>
+        <h1>Man this is awesome</h1>
+      </div>
+    );
   }
 }
 
-export default hot(module)(HelloWorld)
+export default hot(module)(HelloWorld);
